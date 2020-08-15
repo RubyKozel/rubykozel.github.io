5dc5ac4e081cabd9d9a538e7644718cb61cdfcdb
RubyKozel <rubykozel@gmail.com>
2020-08-15 18:14:39 +0300

    Updates

---
+++ precache-manifest.4f3b1a7d53621c7dfa1b4c6401d2fbd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e04fb877b89312a0aaddce04bd5bb65f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee9fcedc232cc83f2f70",
+    "url": "/static/css/main.d702d20f.chunk.css"
+  },
+  {
+    "revision": "af6dfd259f870acd9238",
+    "url": "/static/js/2.2b519426.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2b519426.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee9fcedc232cc83f2f70",
+    "url": "/static/js/main.eef56a9e.chunk.js"
+  },
+  {
+    "revision": "420d11f43645d04fc865",
+    "url": "/static/js/runtime-main.9b126251.js"
+  }
+]);
